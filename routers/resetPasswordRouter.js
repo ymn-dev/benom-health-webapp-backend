3be0819edcf5a314ed737701bfb695bd3c802c16
ymn-dev <sornniyom.a@gmail.com>
2023-10-15 03:35:07 +0700

    now handle reset password via frontend

--- routers/resetPasswordRouter.js
+++ routers/resetPasswordRouter.js
@@ -3,7 +3,7 @@ const express = require("express");
 const User = require("../model/User.js");
 const jwt = require("jsonwebtoken");
 const bcrypt = require("bcrypt");
-
+const nodemailer = require("nodemailer");
 const resetPasswordRouter = express.Router();
 resetPasswordRouter.use(express.json());
 resetPasswordRouter.use(express.urlencoded({ extended: true }));
@@ -14,7 +14,7 @@ const verifyToken = (token) => {
   return decoded;
 };
 
-resetPasswordRouter.get("/", async (req, res, next) => {
+resetPasswordRouter.post("/", async (req, res, next) => {
   try {
     const email = req.body.email;
     const user = await User.findOne({ email }, "email password deleted").lean();
@@ -22,8 +22,30 @@ resetPasswordRouter.get("/", async (req, res, next) => {
       return errorHandler(`email not found`, next, 404);
     }
     //need new email only key here so wont be using createJwt function
-    const token = jwt.sign({ email, usage: user.password }, jwtSecretKey, { expiresIn: "10m" });
-    res.json({ token });
+    const token = jwt.sign({ email, usage: user.password }, jwtSecretKey, { expiresIn: "15m" });
+    const transporter = nodemailer.createTransport({
+      service: "gmail",
+      auth: {
+        user: "benomwebapp@gmail.com",
+        pass: process.env.MAILER_PASSWORD,
+      },
+    });
+    const mailOptions = {
+      from: "benomwebapp@gmail.com",
+      to: email,
+      subject: "Password Reset Request for Benom Webapplication",
+      text: `There was a request for password change, If you did not request a new password, please ignore this email\n
+      click this link to reset password https://benom-health-webapp-frontend.vercel.app/resetpassword?token=${token}`,
+    };
+    transporter.sendMail(mailOptions, function (error, info) {
+      if (error) {
+        res.status(500).json({ error: `some error occured, please try again ${error}` });
+        console.error(error);
+      } else {
+        console.log("Email sent: " + info.response);
+        res.json({ message: `success, please check your email` });
+      }
+    });
   } catch (err) {
     errorHandler(err, next);
   }
@@ -31,7 +53,7 @@ resetPasswordRouter.get("/", async (req, res, next) => {
 
 resetPasswordRouter.patch("/", async (req, res, next) => {
   try {
-    const token = req.query.token;
+    const token = req.body.token;
     const newPassword = req.body.newPassword;
     const result = verifyToken(token);
     if (!result) {
